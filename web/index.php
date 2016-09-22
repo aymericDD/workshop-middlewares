51f821f74928f689d4e12db6b67848d6ed0914f4
Matthieu Napoli <matthieu@mnapoli.fr>
2016-09-22 22:20:08 +0200

    Add $next in callable

--- web/index.php
+++ web/index.php
@@ -1,13 +1,12 @@
 <?php
 
 use Psr\Http\Message\ServerRequestInterface;
-use Superpress\Container;
-use Superpress\Middleware\ErrorHandler;
-use Superpress\Middleware\Pipe;
-use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\Response\TextResponse;
 use Zend\Diactoros\ServerRequestFactory;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // Serve static files
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']))) {
@@ -15,27 +14,14 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . preg_replace('#(\?.*)$
 }
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$container = new Container;
-
 // ---------------------------------------------------------------------------------------------------------------------
 
 
-$application = new Pipe([
-    new ErrorHandler(),
-    function (ServerRequestInterface $request, callable $next) use ($container) {
-        $url = $request->getUri()->getPath();
-        $twig = $container->twig();
-        if ($url === '/') {
-            $latestArticles = $container->articleRepository()->getArticles();
-            return new HtmlResponse($twig->render('home.html.twig', [
-                'articles' => $latestArticles,
-            ]));
-        } elseif ($url === '/about') {
-            return new HtmlResponse($container->twig()->render('about.html.twig'));
-        }
-        return $next($request);
-    }
-]);
+$application = function (ServerRequestInterface $request, callable $next) {
+    $queryParams = $request->getQueryParams();
+    $name = !empty($queryParams['name']) ? $queryParams['name'] : 'world';
+    return new TextResponse('Hello ' . $name . '!');
+};
 
 
 // ---------------------------------------------------------------------------------------------------------------------
