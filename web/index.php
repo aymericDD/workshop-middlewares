1faa837b97f2522f51c83d849056e018249b3c96
Matthieu Napoli <matthieu@mnapoli.fr>
2016-09-22 22:20:08 +0200

    Add $next in callable

--- web/index.php
+++ web/index.php
@@ -1,15 +1,12 @@
 <?php
 
-use Superpress\Container;
-use Superpress\Middleware\ErrorHandler;
-use Superpress\Middleware\HttpBasicAuthentication;
-use Superpress\Middleware\Pipe;
-use Superpress\Middleware\Router;
-use Zend\Diactoros\Response\HtmlResponse;
-use Zend\Diactoros\Response\JsonResponse;
+use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\Response\TextResponse;
 use Zend\Diactoros\ServerRequestFactory;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // Serve static files
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']))) {
@@ -17,37 +14,14 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . preg_replace('#(\?.*)$
 }
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$container = new Container;
-
 // ---------------------------------------------------------------------------------------------------------------------
 
 
-$application = new Pipe([
-    new ErrorHandler(),
-    new Router([
-        '/' => function () use ($container) {
-            $twig = $container->twig();
-            $latestArticles = $container->articleRepository()->getArticles();
-            return new HtmlResponse($twig->render('home.html.twig', [
-                'articles' => $latestArticles,
-            ]));
-        },
-        '/about' => function () use ($container) {
-            return new HtmlResponse($container->twig()->render('about.html.twig'));
-        },
-        '/api/{path:.*}' => new Pipe([
-            new HttpBasicAuthentication(['user' => 'password']),
-            new Router([
-                '/api/articles' => function () use ($container) {
-                    return new JsonResponse($container->articleRepository()->getArticles());
-                },
-                '/api/time' => function () {
-                    return new JsonResponse(time());
-                },
-            ]),
-        ]),
-    ]),
-]);
+$application = function (ServerRequestInterface $request, callable $next) {
+    $queryParams = $request->getQueryParams();
+    $name = !empty($queryParams['name']) ? $queryParams['name'] : 'world';
+    return new TextResponse('Hello ' . $name . '!');
+};
 
 
 // ---------------------------------------------------------------------------------------------------------------------
