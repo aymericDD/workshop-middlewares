b47d419972e132e1fcf7b4e7356ad9ddc484bd4b
Aymericdd <aymeric42680@gmail.com>
2016-11-17 20:18:08 +0100

    Router

--- web/index.php
+++ web/index.php
@@ -1,9 +1,14 @@
 <?php
 
 use Psr\Http\Message\ServerRequestInterface;
+use Superpress\Container;
+use Superpress\Middleware\ErrorHandler;
+use Superpress\Middleware\Pipe;
+use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\Response\TextResponse;
 use Zend\Diactoros\ServerRequestFactory;
+use \Superpress\Middleware\Router;
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -14,14 +19,26 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . preg_replace('#(\?.*)$
 }
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container;
+
 // ---------------------------------------------------------------------------------------------------------------------
 
 
-$application = function (ServerRequestInterface $request, callable $next) {
-    $queryParams = $request->getQueryParams();
-    $name = !empty($queryParams['name']) ? $queryParams['name'] : 'world';
-    return new TextResponse('Hello ' . $name . '!');
-};
+$application = new Pipe([
+    new ErrorHandler(),
+    new Router([
+        '/' => function()  use ($container) {
+            $twig = $container->twig();
+            $latestArticles = $container->articleRepository()->getArticles();
+            return new HtmlResponse($twig->render('home.html.twig', [
+                'articles' => $latestArticles,
+            ]));
+        },
+        '/about' => function() use ($container) {
+            return new HtmlResponse($container->twig()->render('about.html.twig'));
+        }
+    ]),
+]);
 
 
 // ---------------------------------------------------------------------------------------------------------------------
